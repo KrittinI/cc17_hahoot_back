6edd53a3c7cc87951f81e22add28d259c515f402
supakorn roekratchaneekorn <gongsup@supakornsLaptop.home>
2024-07-11 14:06:23 +0700

    i am morning stars

--- src/controllers/comment-controller.js
+++ src/controllers/comment-controller.js
@@ -1,72 +1,71 @@
-const commentService = require("../services/comment-service")
-const questionService = require("../services/question-service")
-const createError = require("../utils/create-error")
+const commentService = require("../services/comment-service");
+const questionService = require("../services/question-service");
+const createError = require("../utils/create-error");
 
-const commentController = {}
-
+const commentController = {};
 
 // ---------------- For Question
 commentController.createCommentQuestion = async (req, res, next) => {
-    try {
-        const { questionId } = req.params
-        const data = req.body
+  try {
+    const { questionId } = req.params;
+    const data = req.body;
 
-        const existQuestion = await questionService.getQuestionByQuestionId(+questionId)
-        if (!existQuestion) {
-            createError(400, "Question not found")
-        }
-
-        data.rate = 5
-        data.questionId = existQuestion.id
-        data.userId = req.user.id
-
-        const comment = await commentService.createQuestionComment(data)
-        console.log(comment);
-        res.status(201).json({ comment })
-    } catch (error) {
-        next(error)
+    const existQuestion = await questionService.getQuestionByQuestionId(+questionId);
+    if (!existQuestion) {
+      createError(400, "Question not found");
     }
-}
+
+    // data.rate = req
+    data.questionId = existQuestion.id;
+    data.userId = req.user.id;
+
+    const comment = await commentService.createQuestionComment(data);
+    console.log(comment);
+    res.status(201).json({ comment });
+  } catch (error) {
+    next(error);
+  }
+};
 
 commentController.editCommentQuestion = async (req, res, next) => {
-    try {
-        console.log(req.params, "edit question")
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    console.log(req.params, "edit question");
+  } catch (error) {
+    next(error);
+  }
+};
 
 commentController.deleteCommentQuestion = async (req, res, next) => {
-    try {
-        console.log(req.params, "delete question");
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    console.log(req.params, "delete question");
+  } catch (error) {
+    next(error);
+  }
+};
 
 // ---------------- For Event
 commentController.createCommentEvent = async (req, res, next) => {
-    try {
-        console.log(req.params, 'comment event');
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    console.log(req.params, "comment event");
+  } catch (error) {
+    next(error);
+  }
+};
 
 commentController.editCommentEvent = async (req, res, next) => {
-    try {
-        console.log(req.params, "edit event")
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    console.log(req.params, "edit event");
+  } catch (error) {
+    next(error);
+  }
+};
 
 commentController.deleteCommentEvent = async (req, res, next) => {
-    try {
-        console.log(req.params, "delete event");
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    console.log(req.params, "delete event");
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = commentController;
